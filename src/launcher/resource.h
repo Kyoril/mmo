2901bf1d6b57af177ab1ffa1bc1fc10d288820e7
Robin Klimonow <r.klimonow@gmail.com>
2021-10-24 13:19:39 +0200

    Add missing resource header

---
+++ src/launcher/resource.h
@@ -0,0 +1,30 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by launcher.rc
+//
+#ifndef IDC_STATIC
+#	define IDC_STATIC				-1
+#endif
+
+#define IDD_DIALOG1                     101
+#define IDI_ICON1                       102
+#define IDB_BITMAP1                     103
+#define IDC_PROGRESS_BAR                1001
+#define IDC_CLOSE                       1002
+#define IDC_PLAY                        1003
+#define IDC_STATUS_LABEL                1004
+#define IDC_OVERALL                     1005
+#define IDC_TOTAL                       1005
+#define IDC_CURRENT                     1006
+#define IDC_CREATE_SHORTCUT             1007
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
