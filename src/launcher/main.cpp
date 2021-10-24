ea72c0d7ab4bc49da4e2acda21221a44d400eb37
Robin Klimonow <r.klimonow@gmail.com>
2021-10-24 12:56:12 +0200

    Fixed launcher label, image and style

--- src/launcher/main.cpp
+++ src/launcher/main.cpp
@@ -198,6 +198,8 @@ namespace mmo
 	}
 }
 
+#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
 	cxxopts::Options options("Available options");
