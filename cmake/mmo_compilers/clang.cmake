43184493aa3083d2c28f10b476688641ce17aaf0
Robin Klimonow <r.klimonow@gmail.com>
2021-12-27 09:55:44 +0100

    Enable c++20 in gcc & clang

--- cmake/mmo_compilers/clang.cmake
+++ cmake/mmo_compilers/clang.cmake
@@ -1,11 +1,11 @@
 
 # This file, when included, does some config options for Clang.
 
-# Enable C++17 standard
+# Enable C++20 standard
 if (APPLE)
-	set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD "c++17")
+	set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LANGUAGE_STANDARD "c++20")
 	set(CMAKE_XCODE_ATTRIBUTE_CLANG_CXX_LIBRARY "libc++")
 endif()
-set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -std=c++17 -stdlib=libc++")
+set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -std=c++20 -stdlib=libc++")
 
 
--- cmake/mmo_compilers/gcc.cmake
+++ cmake/mmo_compilers/gcc.cmake
@@ -1,15 +1,15 @@
 # This file, when included, does some config options for GCC.
 
-# GCC 7.0 or newer is required
+# GCC 8.0 or newer is required
 execute_process(COMMAND ${CMAKE_C_COMPILER} -dumpversion OUTPUT_VARIABLE GCC_VERSION)
-if (NOT (GCC_VERSION VERSION_GREATER 7.0 OR GCC_VERSION VERSION_EQUAL 7.0))
-	message(FATAL_ERROR "GCC 7.0 or newer is required in order to build this project, but you have ${GCC_VERSION}. Please consider an update or switch over to clang.")
+if (NOT (GCC_VERSION VERSION_GREATER 8.0 OR GCC_VERSION VERSION_EQUAL 8.0))
+	message(FATAL_ERROR "GCC 8.0 or newer is required in order to build this project, but you have ${GCC_VERSION}. Please consider an update or switch over to clang.")
 endif()
 
-# Enable C++17 standard
-set (CMAKE_CXX_STANDARD 17)
+# Enable C++20 standard
+set (CMAKE_CXX_STANDARD 20)
 set (CMAKE_CXX_STANDARD_REQUIRED ON)
-list(APPEND CMAKE_CXX_FLAGS "-std=c++17 -pthread")
+list(APPEND CMAKE_CXX_FLAGS "-std=c++20 -pthread")
 
 # Zlib is required for GCC builds
 find_package(ZLIB REQUIRED)
