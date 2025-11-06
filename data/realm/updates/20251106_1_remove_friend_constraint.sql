f086c85daaa50c6719f7efa2a0c9297ee1e92e2e
Robin Klimonow <r.klimonow@gmail.com>
2025-11-06 10:40:29 +0100

    Fix db name in migration

--- data/realm/updates/20251106_1_remove_friend_constraint.sql
+++ data/realm/updates/20251106_1_remove_friend_constraint.sql
@@ -2,7 +2,7 @@
 -- In one-sided friendship system, character_id adds friend_id without automatic reciprocal relationship
 -- The CHECK constraint enforcing character_id < friend_id must be removed
 
-ALTER TABLE `mmo_realm_01`.`history` 
+ALTER TABLE `history` 
 CHANGE COLUMN `id` `id` VARCHAR(64) CHARACTER SET 'latin1' COLLATE 'latin1_german1_ci' NOT NULL ;
 
 ALTER TABLE `friend_list` DROP CHECK `friend_list_chk_1`;
