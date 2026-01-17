e356dac4e9c64f7c3a590ee50843eccc636abdf1
Kyoril <r.klimonow@gmail.com>
2026-01-17 19:19:48 +0100

    Update docs for camera param in Renderable methods
    Added documentation for the camera parameter in PreRender and PostRender methods of mmo::Renderable, clarifying its role in the rendering process.

--- src/shared/scene_graph/renderable.h
+++ src/shared/scene_graph/renderable.h
@@ -40,6 +40,7 @@ namespace mmo
         /// @brief Called just before the renderable is being rendered.
         /// @param scene The scene that this renderable belongs to.
         /// @param graphicsDevice The graphics device that is used for rendering.
+		/// @param camera The camera that is used for rendering.
         /// @return true if the automatic rendering should proceed, false to skip rendering.
         virtual bool PreRender(Scene& scene, GraphicsDevice& graphicsDevice, Camera& camera)
 		{
@@ -49,6 +50,7 @@ namespace mmo
         /// @brief Called immediately after the renderable has been rendered.
         /// @param scene The scene that this renderable belongs to.
         /// @param graphicsDevice The graphics device that was used for rendering.
+		/// @param camera The camera that was used for rendering.
         virtual void PostRender(Scene& scene, GraphicsDevice& graphicsDevice, Camera& camera)
         {
         }
